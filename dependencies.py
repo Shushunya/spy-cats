d04634708a48df1054495fc86b8910aeb9220906
Shushunya <mremarka314@gmail.com>
2025-07-22 19:55:04 +0100

    optimize db dependency

--- dependencies.py
+++ dependencies.py
@@ -1,12 +1,25 @@
-from typing import Annotated
-from fastapi import Depends
+from typing import Annotated, Optional
+
+from fastapi import Depends, Query
 from sqlmodel import Session
-from .database import get_session
+
+from .database import engine
 
 
-async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
-    return {"q": q, "skip": skip, "limit": limit}
+# TODO: add limit: Annotated[int, Query(le=100, ge=1)] = 10
+# Common parameters for all endpoints
+async def common_parameters(
+        offset: int = Query(default=0, ge=0), 
+        limit: int = Query(default=10, ge=1, le=100), 
+        q: Optional[str] = Query(default=None, description="Search q")
+        ):
+    return {"offset": offset, "limit": limit, "q": q}
 
-# List od dependencies
-CommonsDep = Annotated[dict, Depends(common_parameters)] # Common parameters for all endpoints
-SessionDep = Annotated[Session, Depends(get_session)] # Session dependency for database operations
+CommonsDep = Annotated[dict, Depends(common_parameters)]
+
+# Session dependency for database operations
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
